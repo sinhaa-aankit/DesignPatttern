03441c8e4b6c3e0270c9b58a5824993ac2f3842d
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-05-17 09:43:38 +0530

    Added different types for the Engine of the CAR.

--- CarPicker/src/main/kotlin/com/designpattern/carpicker/vehicle/parts/Engine.kt
+++ CarPicker/src/main/kotlin/com/designpattern/carpicker/vehicle/parts/Engine.kt
@@ -1,8 +1,15 @@
 package com.designpattern.carpicker.vehicle.parts
 
-class Engine : Part {
+class Engine (val type : Type) : Part {
     val transmission: Transmission = Transmission()
 
     override val price: Int
-        get() = 150000
+        get() = when(this.type){
+            Type.PETROL -> 150000
+            Type.DIESEL -> 150000
+            Type.HYBRID -> 500000
+            Type.ELECTRIC -> 400000
+        }
+
+    enum class Type{PETROL, DIESEL, HYBRID, ELECTRIC}
 }
