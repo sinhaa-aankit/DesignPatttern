2301cc35ec01929e7a42b5d9778f4f7de31b0dd4
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-05-17 09:39:13 +0530

    Added different types for the WheelBase and Chasis of the CAR.

--- CarPicker/src/main/kotlin/com/designpattern/carpicker/vehicle/parts/Chasis.kt
+++ CarPicker/src/main/kotlin/com/designpattern/carpicker/vehicle/parts/Chasis.kt
@@ -1,9 +1,16 @@
 package com.designpattern.carpicker.vehicle.parts
 
-class Chasis : Part {
+class Chasis (val type : Type) : Part {
     val seats: List<Seat> = listOf(
         Seat(), Seat(), Seat(), Seat()
     )
     override val price: Int
-        get() = 200000
+        get() = when(this.type){
+            Type.HATCHBACK -> 150000
+            Type.SEDAN -> 200000
+            Type.SUV -> 275000
+            Type.PICKUP -> 400000
+        }
+
+    enum class Type {HATCHBACK, SEDAN, SUV, PICKUP}
 }
--- CarPicker/src/main/kotlin/com/designpattern/carpicker/vehicle/parts/WheelBase.kt
+++ CarPicker/src/main/kotlin/com/designpattern/carpicker/vehicle/parts/WheelBase.kt
@@ -1,10 +1,18 @@
 package com.designpattern.carpicker.vehicle.parts
 
-class WheelBase : Part{
-    val chasis: Chasis = Chasis()
+class WheelBase (
+    private val size: WheelBase.Size,
+    val chasis: Chasis
+    ) : Part{
     val wheels: List<Wheel> = listOf(
         Wheel(), Wheel(), Wheel(), Wheel()
     )
     override val price: Int
-        get() = 125000
+        get() = when(this.size){
+            Size.SMALL -> 100000
+            Size.MEDIUM -> 125000
+            Size.BIG -> 150000
+        }
+
+    enum class Size{SMALL, MEDIUM, BIG}
 }
